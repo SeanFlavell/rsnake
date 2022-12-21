77c9ec69ca522c0ecd8616a711659873fb5d48ee
SeanFlavell <119541124+SeanFlavell@users.noreply.github.com>
2022-12-20 21:03:31 -0500

    Add files via upload

---
+++ colors.rs
@@ -0,0 +1,8 @@
+use piston_window::types::Color;
+/// sets the colors of a game object. First three are RGB, and the last number is transparency.
+pub const BACKGROUND: Color = [0.0, 0.0, 0.0, 1.0];
+pub const SCORE: Color = [1.0, 1.0, 1.0, 1.0];
+pub const SNAKE: Color = [0.1, 0.9, 0.1, 1.0];
+pub const FRUIT: Color = [1.0, 0.0, 0.0, 1.0];
+pub const OVERLAY: Color = [1.0, 0.0, 0.0, 0.5];
+pub const SUPFRUIT: Color = [0.0, 0.0, 1.0, 1.0];
